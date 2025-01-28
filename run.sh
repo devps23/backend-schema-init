9c540283e808fd4e299a25578e82e8d600a25d03
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-28 09:31:28 +0530

    schema

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 source /data/secrets.txt
 cat /data/secrets.txt
 
+if [ -z "$DB_HOST" ]; then
+  echo "DB_HOST not exists"
+  exit 1
+fi
 if [ -z "$RDS_DB_HOST" ]; then
   echo "RDS_DB_HOST not exists"
   exit 1
@@ -25,5 +29,5 @@ fi
 git clone https://github.com/devps23/${project_name}-${component}
 cd ${project_name}-$component
 
-mysql -h rds-dev.cvkemucwmc9p.us-east-1.rds.amazonaws.com -umyadmin -pExpenseApp123<schema/backend.sql
+mysql -h${DB_HOST} -u${RDS_DB_USER} -p${$RDS_DB_PASSWORD} <schema/backend.sql
 
