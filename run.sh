0641e98ad3f4157bb8fffbf6870ed4072c376b49
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-18 10:31:31 +0530

    backend schema

--- run.sh
+++ run.sh
@@ -1,30 +1,30 @@
 source /data/secrets.txt
 cat /data/secrets.txt
 
-if [ -z "$DB_HOST" ]; then
+if [ -z "${DB_HOST}" ]; then
   echo "DB_HOST not exists"
   exit 1
 fi
-if [ -z "$RDS_DB_USER" ]; then
+if [ -z "${RDS_DB_USER}" ]; then
   echo "RDS_DB_USER not exists"
   exit 1
 fi
-if [ -z "$RDS_DB_PASSWORD" ]; then
+if [ -z "${RDS_DB_PASSWORD}" ]; then
   echo "RDS_DB_PASSWORD not exists"
   exit 1
 fi
-if [ -z "$component" ]; then
+if [ -z "${component}" ]; then
   echo "component not exists"
   exit 1
 fi
-if [ -z "$project_name" ]; then
+if [ -z "${project_name}" ]; then
   echo "project_name not exists"
   exit 1
 fi
 
 git clone https://github.com/devps23/${project_name}-${component}
-cd ${project_name}-$component
-
+cd ${project_name}-${component}
+echo ${project_name}-${component}
 mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
 echo mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
 
