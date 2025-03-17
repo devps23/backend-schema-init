c557a9da16a550927cee75407b57746fdbaadc68
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-17 16:24:51 +0530

    roboshop catalogue

--- run.sh
+++ run.sh
@@ -1,30 +1,39 @@
 source /data/secrets.txt
 cat /data/secrets.txt
+ git clone https://github.com/devps23/${project_name}-${component}
+  cd ${project_name}-${component}
+if [ "${db_type}" == "mongodb" ]; then
+  echo ${project_name}-${component}
+  curl -L -o https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem /app/global-bundle.pem
+   mongosh docdb.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile global-bundle.pem --retryWrites=false --username docdb --password roboshop12345 </app/schema/catalogue.sql
+fi
+if [ "${db_type}" == "mysql" ]; then
+  if [ -z "${DB_HOST}" ]; then
+    echo "DB_HOST not exists"
+    exit 1
+  fi
+  if [ -z "${RDS_DB_USER}" ]; then
+    echo "RDS_DB_USER not exists"
+    exit 1
+  fi
+  if [ -z "${RDS_DB_PASSWORD}" ]; then
+    echo "RDS_DB_PASSWORD not exists"
+    exit 1
+  fi
+  if [ -z "${component}" ]; then
+    echo "component not exists"
+    exit 1
+  fi
+  if [ -z "${project_name}" ]; then
+    echo "project_name not exists"
+    exit 1
+  fi
 
-if [ -z "${DB_HOST}" ]; then
-  echo "DB_HOST not exists"
-  exit 1
-fi
-if [ -z "${RDS_DB_USER}" ]; then
-  echo "RDS_DB_USER not exists"
-  exit 1
-fi
-if [ -z "${RDS_DB_PASSWORD}" ]; then
-  echo "RDS_DB_PASSWORD not exists"
-  exit 1
-fi
-if [ -z "${component}" ]; then
-  echo "component not exists"
-  exit 1
-fi
-if [ -z "${project_name}" ]; then
-  echo "project_name not exists"
-  exit 1
+  git clone https://github.com/devps23/${project_name}-${component}
+  cd ${project_name}-${component}
+  echo ${project_name}-${component}
+  mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
+  echo mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
 fi
 
-git clone https://github.com/devps23/${project_name}-${component}
-cd ${project_name}-${component}
-echo ${project_name}-${component}
-mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
-echo mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
 
