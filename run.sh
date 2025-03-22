d30bbf2f053dda0c6d5795b2e73ab7203bb4b09d
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-22 15:28:21 +0530

    roboshop

--- run.sh
+++ run.sh
@@ -1,17 +1,4 @@
 source /data/secrets.txt
-cat /data/secrets.txt
-echo
-echo
-echo
-env
- git clone https://github.com/devps23/${project_name}-${component}
-  cd ${project_name}-${component}
-if [ "${db_type}" == "mongodb" ]; then
-  echo ${project_name}-${component}
-  curl -L -o https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem /app/global-bundle.pem
-   mongosh docdb.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile global-bundle.pem --retryWrites=false --username docdb --password roboshop12345 </app/schema/catalogue.sql
-fi
-if [ "${db_type}" == "mysql" ]; then
   if [ -z "${DB_HOST}" ]; then
     echo "DB_HOST not exists"
     exit 1
@@ -38,6 +25,19 @@ if [ "${db_type}" == "mysql" ]; then
   echo ${project_name}-${component}
   mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
   echo mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
-fi
+
+#cat /data/secrets.txt
+#echo
+#echo
+#echo
+#env
+# git clone https://github.com/devps23/${project_name}-${component}
+#  cd ${project_name}-${component}
+#if [ "${db_type}" == "mongodb" ]; then
+#  echo ${project_name}-${component}
+#  curl -L -o https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem /app/global-bundle.pem
+#   mongosh docdb.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile global-bundle.pem --retryWrites=false --username docdb --password roboshop12345 </app/schema/catalogue.sql
+#fi
+
 
 
