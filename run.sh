8495d67d1fbdde8cb2d4dee75de9e2f7aa35ff28
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-23 11:01:46 +0530

    added mongodb

--- run.sh
+++ run.sh
@@ -1,30 +1,39 @@
 source /data/secrets.txt
-  if [ -z "${DB_HOST}" ]; then
-    echo "DB_HOST not exists"
-    exit 1
-  fi
-  if [ -z "${RDS_DB_USER}" ]; then
-    echo "RDS_DB_USER not exists"
-    exit 1
-  fi
-  if [ -z "${RDS_DB_PASSWORD}" ]; then
-    echo "RDS_DB_PASSWORD not exists"
-    exit 1
-  fi
-  if [ -z "${component}" ]; then
-    echo "component not exists"
-    exit 1
-  fi
-  if [ -z "${project_name}" ]; then
-    echo "project_name not exists"
-    exit 1
-  fi
-
-  git clone https://github.com/devps23/${project_name}-${component}
-  cd ${project_name}-${component}
-  echo ${project_name}-${component}
+git clone https://github.com/devps23/${project_name}-${component}
+cd ${project_name}-${component}
+if [ ${db_type} == "mongodb" ]; then
+   curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+   mongodb://docdb:roboshop12345@docdb.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false
+   mongosh docdb.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile global-bundle.pem --retryWrites=false --username docdb --password roboshop12345 </app/catalogue.js
+fi
+if [ ${db_type} == "mysql" ]; then
+    if [ -z "${DB_HOST}" ]; then
+        echo "DB_HOST not exists"
+        exit 1
+      fi
+      if [ -z "${RDS_DB_USER}" ]; then
+        echo "RDS_DB_USER not exists"
+        exit 1
+      fi
+      if [ -z "${RDS_DB_PASSWORD}" ]; then
+        echo "RDS_DB_PASSWORD not exists"
+        exit 1
+      fi
+      if [ -z "${component}" ]; then
+        echo "component not exists"
+        exit 1
+      fi
+      if [ -z "${project_name}" ]; then
+        echo "project_name not exists"
+        exit 1
+      fi
   mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
   echo mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
+fi
+
+
+  echo ${project_name}-${component}
+
 
 #cat /data/secrets.txt
 #echo
