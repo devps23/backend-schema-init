8a602f2a67cb0d4f46b7ed1f1a9a6d73589f8882
Your Name <vemasani.pavani@gmail.com>
2025-04-14 14:50:34 +0530

    expense

--- run.sh
+++ run.sh
@@ -1,42 +1,69 @@
 source /data/secrets.txt
 cat /data/secrets.txt
-echo
-echo
-echo
-env
+
+if [ -z "${DB_HOST}" ]; then
+  echo "DB_HOST not exists"
+  exit 1
+fi
+if [ -z "${RDS_DB_USER}" ]; then
+  echo "RDS_DB_USER not exists"
+  exit 1
+fi
+if [ -z "${RDS_DB_PASSWORD}" ]; then
+  echo "RDS_DB_PASSWORD not exists"
+  exit 1
+fi
+if [ -z "${component}" ]; then
+  echo "component not exists"
+  exit 1
+fi
+if [ -z "${project_name}" ]; then
+  echo "project_name not exists"
+  exit 1
+fi
+
 git clone https://github.com/devps23/${project_name}-${component}
 cd ${project_name}-${component}
- echo ${project_name}-${component}
-if [ "${db_type}" == 'mongodb' ]; then
-    wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-    mongo --tls --host my-docdb-cluster.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tlsCAFile global-bundle.pem --username docdb --password roboshop123 <schema/${component}.js
-
-fi
-if [ "${db_type}" == 'mysql' ]; then
-    if [ -z "${DB_HOST}" ]; then
-        echo "DB_HOST not exists"
-        exit 1
-      fi
-      if [ -z "${RDS_DB_USER}" ]; then
-        echo "RDS_DB_USER not exists"
-        exit 1
-      fi
-      if [ -z "${RDS_DB_PASSWORD}" ]; then
-        echo "RDS_DB_PASSWORD not exists"
-        exit 1
-      fi
-      if [ -z "${component}" ]; then
-        echo "component not exists"
-        exit 1
-      fi
-      if [ -z "${project_name}" ]; then
-        echo "project_name not exists"
-        exit 1
-      fi
-
-  mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/${component}.sql
-  echo mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/${component}.sql
-fi
+echo ${project_name}-${component}
+mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
+echo mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/backend.sql
+#echo
+#echo
+#echo
+#env
+#git clone https://github.com/devps23/${project_name}-${component}
+#cd ${project_name}-${component}
+# echo ${project_name}-${component}
+#if [ "${db_type}" == 'mongodb' ]; then
+#    wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+#    mongo --tls --host my-docdb-cluster.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tlsCAFile global-bundle.pem --username docdb --password roboshop123 <schema/${component}.js
+#
+#fi
+#if [ "${db_type}" == 'mysql' ]; then
+#    if [ -z "${DB_HOST}" ]; then
+#        echo "DB_HOST not exists"
+#        exit 1
+#      fi
+#      if [ -z "${RDS_DB_USER}" ]; then
+#        echo "RDS_DB_USER not exists"
+#        exit 1
+#      fi
+#      if [ -z "${RDS_DB_PASSWORD}" ]; then
+#        echo "RDS_DB_PASSWORD not exists"
+#        exit 1
+#      fi
+#      if [ -z "${component}" ]; then
+#        echo "component not exists"
+#        exit 1
+#      fi
+#      if [ -z "${project_name}" ]; then
+#        echo "project_name not exists"
+#        exit 1
+#      fi
+#
+#  mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/${component}.sql
+#  echo mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/${component}.sql
+#fi
 
 
 
